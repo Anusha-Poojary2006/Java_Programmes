76de356be787c2b0139b0bdfb1a55d2f61cd358a
Anusha <anushadharemane@gmail.com>
2026-04-07 16:56:24 +0530

    Add files via upload

---
+++ reverseString.java
@@ -0,0 +1,21 @@
+//java programme to reverse the string using recursion
+
+import java.util.Scanner;
+
+public class reverseString {
+    public static String fn(String s1){
+        if(s1.length()==0){
+            return "";
+        }
+        char ch=s1.charAt(s1.length()-1);
+        return ch+fn(s1.substring(0,s1.length()-1));
+
+    }
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a string:");
+    String s1=sc.nextLine();
+    System.out.println("Reverse of the given string is "+fn(s1));
+    sc.close();
+    }
+}
